f01a76bc3ddcc6e726718792a68999792e2c9b42
Denys Koval <denys_koval@epam.com>
2023-05-31 16:15:17 +0300

    Add project files.

---
+++ Constants/Authorization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServices.Constants
+{
+    public static class Authorization
+    {
+        public static string Username => "admin";
+
+        public static string Password => "password123";
+    }
+}
